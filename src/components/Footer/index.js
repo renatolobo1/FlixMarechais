0c9e095f730f56833d436c31feac8e7fde66d044
Renato Lôbo Guedes Alcoforado <renatoloboguedes@gmail.com>
2020-07-30 10:16:50 -0300

    Termino da aula 2

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FooterBase } from './styles';
+import LogoMarechais from '../../assets/img/LogoMarechais.png'
+
 
 function Footer() {
   return (
     <FooterBase>
+      <Link to="/">
+          <img className="Logo" src={LogoMarechais} alt="MarechaisFlix logo" />
+      </Link>
       
+     
       <p>
         Associação Desportista Maceio Marechais
         {' '}
